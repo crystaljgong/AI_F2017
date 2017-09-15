f477790aa3c3d32713b7d861ef7444dd16f16dc0
Cynthia Zheng <xz7uy@virginia.edu>
2017-09-15 10:39:37 -0400

    update

--- AI.py
+++ AI.py
@@ -10,14 +10,14 @@ def add_definition(name, definition, root, fact):
 def List(variables, facts, rules):
 	#print root vars
 	print("Root Variables")
-	for key, value in variables.iteritems():
+	for key, value in variables.items():
 		if value[1]:
 			print("    {} = {}".format(key, value[0]))
 
 	#print learned vars
 	#this not v efficient b/c looping twice but whatever
 	print("Learned Variables")
-	for key, value in variables.iteritems():
+	for key, value in variables.items():
 		poop = []
 		if not value[1]:
 			print("    {} = {}".format(key, value[0]))
@@ -36,7 +36,35 @@ def List(variables, facts, rules):
 
 
 def main():
-	add_definition("S", "Sam likes ice cream", True, True)
+	s = input('Enter a command: ')
+	inp = s.split()
+
+	if inp[0] == "Teach":
+		if inp[2] == "=":
+			if inp[1] in facts:
+				print("Error: cannotset a learned variable directly")
+			else:
+				#observeRootVar(inp[1], inp[3])
+				facts.append(inp[1])
+		elif inp[2] == "->":
+			if inp[3] in facts:
+				print("ah")
+				#createNewRule(inp[1], inp[3])
+		else:
+			string = s.split("=")
+			add_definition(inp[2], string[1], inp[1], False)
+
+	elif inp[0] == "List":
+		List(variables, facts, rules)
+	elif inp[0] == "Learn":
+		print("Learn()")
+	elif inp[0] == "Query":
+		print("#Query(inp[1])")
+	elif inp[0] == "Why":
+		print("#Why(inp[1])")
+
+
+	add_definition("S", "\"Sam likes ice cream\"", True, True)
 	add_definition("V", "Today is Sunday", True, True)
 	add_definition("EAT", "Sam will eat ice cream", False, True)
 	add_definition("F", "Python is fun", False, True)
--- input.py
+++ input.py
@@ -1,30 +1,28 @@
 def main():
-   s = input('Enter a command: ')
-   input = s.split()
+	s = input('Enter a command: ')
+	input = s.split()
 
-   if input[0] == "Teach":
-       if input[2] == "=":
-           if input[1] in facts: #?learned var?
-                print("Error: cannot set a learned variable directly")
-           else:
-           # observeRootVar(input[1], input[3])
-                facts.append(input[1])
-       elif input[2] == "->":
-           if input[3] in facts:
-            # createNewRule(input[1], input[3])
+	if input[0] == "Teach":
+		if input[2] == "=":
+			if input[1] in facts:
+				print("Error: cannotset a learned variable directly")
+			else:
+				#observeRootVar(input[1], input[3])
+				facts.append(input[1])
+		elif input[2] == "->":
+			if input[3] in facts:
+				print("ah")
+				#createNewRule(input[1], input[3])
+		else:
+			string = s.split("=")
+			add_definition(input[2], string[1], input[1], false)
 
-       else:
-           string = s.split("=")
-           add_definition(input[2], string[1], input[1], false) #root is ARG?
-
-
-    elif input[0] == "List":
-        List()
-    elif input[0] == "Learn":
-        # Learn()
-    elif input[0] == "Query":
-        # Query(input[1])
-    elif input[0] == "Why":
-        # Why(input[1])
-
+	elif input[0] == "List":
+		List()
+	elif input[0] == "Learn":
+		print("Learn()")
+	elif input[0] == "Query":
+		print("#Query(input[1])")
+	elif input[0] == "Why":
+		print("#Why(input[1])")
     
